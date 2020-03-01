3821117bcd7b685f09f26ebba53bec4a224df0f1
Shureek Yunusov <mopkobka97@yandex.ru>
2020-03-01 03:07:21 +0300

    pom, tests refactoring

--- src/test/java/com/itmo/wst/WineWebServiceApplicationIntegrationTests.java
+++ src/test/java/com/itmo/wst/WineWebServiceApplicationIntegrationTests.java
@@ -21,7 +21,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class ProducingWebServiceApplicationIntegrationTests {
+public class WineWebServiceApplicationIntegrationTests {
 
 	@BeforeEach
 	public void init() throws Exception {
