d913639d78bb15ae152a87a0b17f960e5c230ab1
Oleg Romanov <romanov-97@mail.ru>
2020-06-28 20:24:33 +0300

    Add basic auth to client

--- client/src/main/java/com/itmo/wst/WineClient.java
+++ client/src/main/java/com/itmo/wst/WineClient.java
@@ -1,9 +1,12 @@
 package com.itmo.wst;
 
 import java.math.BigInteger;
+import java.nio.charset.Charset;
 
 import com.itmo.wst.model.*;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -15,32 +18,51 @@ public class WineClient {
         this.restTemplate = restTemplate;
     }
 
-    public GetWineResponse getWine(Wine wine) {
-        GetWineRequest request = new GetWineRequest();
-        request.setWine(wine);
+    HttpHeaders createHeaders(String username, String password){
+        return new HttpHeaders() {{
+            String auth = username + ":" + password;
+            byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+            String authHeader = "Basic " + new String(encodedAuth);
+            set("Authorization", authHeader);
+        }};
+    }
 
-        return restTemplate.postForObject(
-                "http://localhost:8080/rest/wines/get", request, GetWineResponse.class
+    public GetWineResponse getWine(Wine wine) {
+        GetWineRequest wineRequest = new GetWineRequest();
+        wineRequest.setWine(wine);
+
+        HttpEntity<GetWineRequest> request = new HttpEntity<GetWineRequest>(
+                wineRequest, createHeaders("admin", "{noop}password")
         );
+        ResponseEntity<GetWineResponse> response = restTemplate
+                .exchange("http://localhost:8080/rest/wines/get", HttpMethod.POST, request, GetWineResponse.class);
+
+        return response.getBody();
     }
 
     public CreateWineResponse createWine(Wine wine) {
-        CreateWineRequest request = new CreateWineRequest();
-        request.setName(wine.getName());
-        request.setColor(wine.getColor());
-        request.setRating(wine.getRating());
-        request.setSugar(wine.getSugar());
+        CreateWineRequest wineRequest = new CreateWineRequest();
+        wineRequest.setName(wine.getName());
+        wineRequest.setColor(wine.getColor());
+        wineRequest.setRating(wine.getRating());
+        wineRequest.setSugar(wine.getSugar());
 
-        return restTemplate.postForObject(
-                "http://localhost:8080/rest/wines", request, CreateWineResponse.class
+        HttpEntity<CreateWineRequest> request = new HttpEntity<CreateWineRequest>(
+                wineRequest, createHeaders("admin", "{noop}password")
         );
+        ResponseEntity<CreateWineResponse> response = restTemplate
+                .exchange("http://localhost:8080/rest/wines", HttpMethod.POST, request, CreateWineResponse.class);
+
+        return response.getBody();
     }
 
     public UpdateWineResponse updateWine(Wine wine) {
         UpdateWineRequest wineRequest = new UpdateWineRequest();
         wineRequest.setWine(wine);
 
-        HttpEntity<UpdateWineRequest> request = new HttpEntity<>(wineRequest);
+        HttpEntity<UpdateWineRequest> request = new HttpEntity<UpdateWineRequest>(
+                wineRequest, createHeaders("admin", "{noop}password")
+        );
         ResponseEntity<UpdateWineResponse> response = restTemplate
                 .exchange("http://localhost:8080/rest/wines", HttpMethod.PUT, request, UpdateWineResponse.class);
 
@@ -51,7 +73,9 @@ public class WineClient {
         DeleteWineRequest wineRequest = new DeleteWineRequest();
         wineRequest.setId(id);
 
-        HttpEntity<DeleteWineRequest> request = new HttpEntity<>(wineRequest);
+        HttpEntity<DeleteWineRequest> request = new HttpEntity<>(
+                wineRequest, createHeaders("admin", "{noop}password")
+        );
         ResponseEntity<DeleteWineResponse> response = restTemplate
                 .exchange("http://localhost:8080/rest/wines", HttpMethod.DELETE, request, DeleteWineResponse.class);
 
